de58b1b84d16aedd21ccdabb4a326215d6974e7a
Zach Dudzik <32280363+zdudzik@users.noreply.github.com>
2019-06-20 12:51:35 -0400

    security

--- vbot/vbot.py
+++ vbot/vbot.py
@@ -12,7 +12,7 @@ bearer_token = 'qrfhyetwutnnucb3uuoe3ka1fh'
 
 LISTEN_URL = 'https://mattermost.hyland.com/api/v4/channels/' + bearer_token + '/posts'
 POST_URL = 'https://mattermost.hyland.com/api/v4/posts'
-AUTH_TOKEN = 'h6s5cmnp5pr77kx6s7ao3huwzh'
+AUTH_TOKEN = '--------------------------' #intercept traffic using fiddler and paste auth_token here
 
 #Checks for valid commands
 def process_commands(message, name):
